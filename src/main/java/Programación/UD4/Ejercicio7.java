c0ff789728d7a245102b40f95f880845520d17f4
AarLozMer <aarlozmer@alu.edu.gva.es>
2024-10-28 13:47:54 +0100

    Commit Inicial

--- src/main/java/Programación/UD4/Ejercicio7.java
+++ src/main/java/Programación/UD4/Ejercicio7.java
@@ -1,4 +1,15 @@
 package Programación.UD4;
 
+import java.util.Scanner;
+
 public class Ejercicio7 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Introduce un valor para la base: ");
+        float b = input.nextFloat();
+        System.out.print("Introduce un valor para la altura: ");
+        float h = input.nextFloat();
+
+    }
 }
